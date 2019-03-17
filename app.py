085a66d18dd713d938b20947005852ed605e11cd
AugNSo <33392231+AugNSo@users.noreply.github.com>
2019-03-17 10:46:31 +0800

    Post function finished

--- app.py
+++ app.py
@@ -8,57 +8,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://smt203t1:smt203t1@localhos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Professor,Course,Prof_Course,Review
+from models import Professor, Course, Prof_Course, Review
 
-@app.route('/postprofessor',methods=["POST"])
+@app.route('/postprofessor', methods=["POST"])
 def create_prof():
     name = request.json['name']
-    prof_course = request.json['prof_course']
     new_prof = Professor(name=name)
     db.session.add(new_prof)
     db.session.commit()
+    return jsonify('{} was created'.format(new_prof))
 
-@app.route('/postcourse',methods=["POST"])
+
+@app.route('/postcourse', methods=["POST"])
 def create_course():
     cid = request.json['cid']
     name = request.json['name']
     school = request.json['school']
-    prof_course = request.json['prof_course']
-    new_course = Course(cid=cid,name=name,school=school)
+    new_course = Course(cid=cid, name=name, school=school)
     db.session.add(new_course)
     db.session.commit()
-    for p in prof_course:
-        curr_prof = Professor.query.filter_by(name=p).first()
-        if curr_prof is None:
-            new_prof = Professor(name=p,prof_course=[new_course])
-            db.session.add(new_prof)
-        else:
-            curr_prof.prof_course.append(new_course)
-    db.session.commit()
-    for p in prof_course:
-        new_prof_course = Prof_Course(cname=name,pname=p)
-        db.session.add(new_prof_course)
-        db.session.commit()
+    return jsonify('{} was created'.format(new_course))
 
-@app.route('/postprofcourse',methods=["POST"])
+
+@app.route('/postprofcourse', methods=["POST"])
 def create_profcouse():
-    cid = request.json['cid']
-    pid = request.json['pid']
-    """
+    #cid = request.json['cid']
+    #pid = request.json['pid']
     cname = request.json['cname']
     pname = request.json['pname']
-    
+
     new_profcourse = Prof_Course(cname=cname, pname=pname)
-    """
-    new_profcourse = Prof_Course(cid=cid, pid=pid)
+    #new_profcourse = Prof_Course(cid=cid, pid=pid)
     db.session.add(new_profcourse)
     db.session.commit()
+    return jsonify('{} was created'.format(new_profcourse))
+
 
 @app.route('/postreview', methods=["POST"])
 def create_postreview():
     reviewer = request.json['reviewer']
-    professor = request.json['pid']
-    course = request.json['cid']
+    pname = request.json['pname']
+    cname = request.json['cname']
     score1 = request.json['score1']
     score2 = request.json['score2']
     score3 = request.json['score3']
@@ -78,10 +68,11 @@ def create_postreview():
         advice = request.json['advice']
     except:
         advice = None
-    new_review = Review(reviewer=reviewer, pid=pid, cid=cid, score1=score1, score2=score2, score3=score3, year=year, school=school, comment=comment, advice=advice)
+    new_review = Review(reviewer=reviewer, pname=pname, cname=cname, score1=score1,
+                        score2=score2, score3=score3, year=year, school=school, comment=comment, advice=advice)
     db.session.add(new_review)
     db.session.commit()
-
+    return jsonify('{} was created'.format(new_review))
 
 
 # your code ends here
--- models.py
+++ models.py
@@ -5,12 +5,12 @@ class Course(db.Model):
     __tablename__ = "course"
     id = db.Column(db.Integer, primary_key=True)
     cid = db.Column(db.String(20), nullable=False)
-    name = db.Column(db.String(80), nullable=False,unique=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
     school = db.Column(db.String(10), nullable=False)
     prof_course = db.relationship(
         'Prof_Course', back_populates='course', cascade='all', lazy=True, uselist=True)
 
-    def __init__(self, cid, name, school,prof_course=None):
+    def __init__(self, cid, name, school, prof_course=None):
         self.cid = cid
         self.name = name
         self.school = school
@@ -21,7 +21,7 @@ class Course(db.Model):
 class Professor(db.Model):
     __tablename__ = "professor"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False,unique=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
     prof_course = db.relationship(
         'Prof_Course', back_populates='professor', cascade='all', lazy=True, uselist=True)
 
@@ -34,16 +34,17 @@ class Professor(db.Model):
 class Prof_Course(db.Model):
     __tablename__ = "prof_course"
     cname = db.Column(db.String(80), db.ForeignKey('course.name'))
-    pname = db.Column(db.String(80),db.ForeignKey('professor.name'))
+    pname = db.Column(db.String(80), db.ForeignKey('professor.name'))
     professor = db.relationship('Professor', back_populates='prof_course')
     course = db.relationship('Course', back_populates='prof_course')
     review = db.relationship(
         'Review', back_populates='prof_course', cascade='all', lazy=True, uselist=True)
-    __table_args__ = (db.PrimaryKeyConstraint('cname','pname',name='prof_course_pk'),{})
+    __table_args__ = (db.PrimaryKeyConstraint(
+        'cname', 'pname', name='prof_course_pk'), {})
 
-    def __init__(self, cname,pname,review=None):
-        self.cname=cname
-        self.pname=pname
+    def __init__(self, cname, pname, review=None):
+        self.cname = cname
+        self.pname = pname
         review = [] if review is None else review
         self.review = review
 
@@ -56,25 +57,22 @@ class Review(db.Model):
     score1 = db.Column(db.Float, nullable=False)
     score2 = db.Column(db.Float, nullable=False)
     score3 = db.Column(db.Float, nullable=False)
-    year = db.Column(db.Integer,nullable=True)
-    school = db.Column(db.String(10),nullable=True)
-    comment = db.Column(db.String(300),nullable=True)
-    advice = db.Column(db.String(300),nullable=True)
-    __table_args__ = (db.ForeignKeyConstraint(['pname','cname'],['prof_course.pname','prof_course.cname']),)
+    year = db.Column(db.Integer, nullable=True)
+    school = db.Column(db.String(10), nullable=True)
+    comment = db.Column(db.String(300), nullable=True)
+    advice = db.Column(db.String(300), nullable=True)
+    __table_args__ = (db.ForeignKeyConstraint(['pname', 'cname'], [
+                      'prof_course.pname', 'prof_course.cname']),)
     prof_course = db.relationship('Prof_Course', back_populates='review')
 
-    def __init__(self, reviewer, pname, cname, score1, score2, score3, year=None, school=None, comment=None,advice=None):
+    def __init__(self, reviewer, pname, cname, score1, score2, score3, year=None, school=None, comment=None, advice=None):
         self.reviewer = reviewer
         self.pname = pname
         self.cname = cname
         self.score1 = score1
         self.score2 = score2
         self.score3 = score3
-        school = [] if school is None else school
         self.school = school
-        year = [] if year is None else year
         self.year = year
-        comment = [] if comment is None else comment
         self.comment = comment
-        advice = [] if comment is None else advice
         self.advice = advice
